c11074b54d56eefd2f6d7c4b8d3398ebfa3c34f5
Phil <phil@pcollins.tech>
2019-09-12 09:36:38 +0100

    remove location from layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,22 +1,18 @@
-import React, { Fragment } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import MainNav from "./MainNav";
-import Footer from "./Footer";
-import "../sass/app.scss";
+import React, { Fragment } from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import MainNav from "./MainNav"
+import Footer from "./Footer"
+import "../sass/app.scss"
 const Layout = ({ props, children }) => {
-  
-    const { location } = props;
-    return (
-        <Fragment>
-            <MainNav router={props} />
-            {/* <div className="container" style={{marginTop: "80px"}} > */}
-            <div className={location.pathname !== "/" ? "not-home" : ""}>
-                {children}
-            </div>
-            {/* </div> */}
-            <Footer />
-        </Fragment>
-    );
-};
+  return (
+    <Fragment>
+      <MainNav router={props} />
+      {/* <div className="container" style={{marginTop: "80px"}} > */}
+      {children}
+      {/* </div> */}
+      <Footer />
+    </Fragment>
+  )
+}
 
-export default Layout;
+export default Layout
