8f7c53dd5f4129236084987866f3074e512d48be
Phil <phil@pcollins.tech>
2019-12-15 09:59:10 +0000

    Remove G-Science

--- src/components/seo.js
+++ src/components/seo.js
@@ -36,10 +36,6 @@ function SEO({ description, lang, meta, title }) {
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
         {
-          name: `google-site-verification`,
-          content: "tqvPcvUgNFlRb4bUHeDJd4ElDY1T7CECHf0pUcHi118",
-        },
-        {
           name: `description`,
           content: metaDescription,
         },
